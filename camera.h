23f4102d2319edd7d6b2ad14b88d359917c35f55
Winter Guerra <mr.servergeek@gmail.com>
2012-01-17 03:44:52 -0500

    Updated the code so that it works with the newest version of Arduino (V1)

--- camera.h
+++ camera.h
@@ -1,3 +1,20 @@
+//This is a part of the source code I coded for a xbee and stepper-motor
+//driven camera mount needed for a school play. More info on the current 
+//status of the project can be found at its thingiverse page here: 
+//http://www.thingiverse.com/thing:16092
+//or at my code's Github repo here: 
+//https://github.com/xtremd/Parametric-Openscad-Tripod-Mount
+//and here:
+//https://github.com/xtremd/XbeeRemoteCameraRig
+
+//This code is released under the CC-BY-SA license. Read more about it here:
+//http://creativecommons.org/licenses/by-sa/3.0/us/
+
+//Go be awesome!
+
+//-XtremD
+//January 17th, 2012
+
 #ifndef CAMERA_H
 #define CAMERA_H
 
--- settings.h
+++ settings.h
@@ -1,7 +1,24 @@
+//This is a part of the source code I coded for a xbee and stepper-motor
+//driven camera mount needed for a school play. More info on the current 
+//status of the project can be found at its thingiverse page here: 
+//http://www.thingiverse.com/thing:16092
+//or at my code's Github repo here: 
+//https://github.com/xtremd/Parametric-Openscad-Tripod-Mount
+//and here:
+//https://github.com/xtremd/XbeeRemoteCameraRig
+
+//This code is released under the CC-BY-SA license. Read more about it here:
+//http://creativecommons.org/licenses/by-sa/3.0/us/
+
+//Go be awesome!
+
+//-XtremD
+//January 17th, 2012
+
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#define IS_CONTROLLER 1 //Is this the camera controller? Or the reciever?
+#define IS_CONTROLLER 0 //Is this the camera controller? Or the reciever?
 #define DEBUG_MODE IS_CONTROLLER && 1 //Do we want debug output on the serial line?
 
 
